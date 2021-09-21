9c0609b6cded26aa48dd6a517e7e25d89bb5c1b5
samrshi <samrshi@outlook.com>
2021-09-21 17:24:53 -0400

    Initial commit

---
+++ p00-weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  p00-weather
+//
+//  Created by Samuel Shi on 9/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().preferredColorScheme(.dark)
+    }
+}
